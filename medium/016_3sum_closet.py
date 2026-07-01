16b55a5346deb63758174ce60a7eac824dfc67ab
Sehr Abrar <138559746+sehr-abrar@users.noreply.github.com>
2026-06-30 22:11:50 -0400

    Implement threeSumClosest function for closest sum

---
+++ medium/016_3sum_closet.py
@@ -0,0 +1,26 @@
+# 16. Medium: 3Sum Closet
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+
+        closest = nums[0] + nums[1] + nums[2]
+
+        for i in range(len(nums) - 2):
+            l = i + 1
+            r = len(nums) - 1
+
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+
+                if abs(total - target) < abs(closest - target):
+                    closest = total
+
+                if total < target:
+                    l += 1
+                elif total > target:
+                    r -= 1
+                else:
+                    return target
+
+        return closest
